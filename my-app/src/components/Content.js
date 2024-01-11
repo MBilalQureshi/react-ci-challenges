e9326581eb245b64e9baab06aff8c525b56cf883
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-01-11 18:09:48 +0000

    Implement component lifecycle challenge, component did mount

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,15 +2,29 @@ import css from './css/Content.module.css'
 import React, { Component } from 'react'
 import {savedPosts} from '../posts.json'
 import PostItem from './PostItem'
+import Loader from './Loader'
 
 export class Content extends Component {
     constructor(props) {
       super(props)
     
       this.state = {
-         
+         isLoaded: true,
       }
     }
+
+    isContentLoaded(){
+      setTimeout(()=>{
+        this.setState({
+          isLoaded : false,
+        })
+      },2000)
+    }
+
+    componentDidMount(){
+      this.isContentLoaded();
+    }
+
   render() {
     return (
       <div>
@@ -18,7 +32,8 @@ export class Content extends Component {
             <h1>My Photos</h1>
         </div>
         <div className={css.SearchResults}>
-           <PostItem savedPosts={savedPosts} css={css}/>
+          {this.state.isLoaded ? (<Loader />): (<PostItem savedPosts={savedPosts} css={css}/>)}
+           
         </div>
       </div>
     )
